899d7354e7477cb880d88a476d3ee558e48c0b2e
Novendra Anugrah <novendraanugrah@gmail.com>
2025-09-11 19:48:43 +0700

    Create Companions Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to My SaaS App</h1>
-      <Button>
-        Let`s Get Started
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#bde8ff"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList 
+          title="Recently completed session"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
